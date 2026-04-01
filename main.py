53e2a16a74a25b796ce89bfe5a13857ed6f9ee02
lachlanwilson <wilson2l03@stirlingschools.net>
2026-04-01 17:10:23 +0100

    Added titles and made it look good.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.style.use("./style.mlpstyle")
+
 
 class MultilayerPerceptron:
 
@@ -8,60 +10,80 @@ class MultilayerPerceptron:
         self.sizes = sizes
 
         random_gen = np.random.default_rng(42)
-        plt.rcParams['text.usetex'] = True
-
         self.perceptrons = [random_gen.random(i, dtype=np.float32) for i in sizes]
         self.paths = [random_gen.random((sizes[i] * sizes[i + 1], 2), dtype=np.float32) for i in range(len(sizes) - 1)]
 
-    def display(self, max_height=None):
+    def show_background(self, ax, max_height=None, layer_width=6):
+        x_offset = layer_width / 2
 
+        ax.add_patch(plt.Rectangle((0, 0), layer_width, max_height + 4.2, color=(0, 0, 0, 0.05)))
+        ax.text(x_offset, max_height + 3, "Input Layer", va="center", ha="center")
+        ax.add_patch(plt.Rectangle((layer_width, 0), layer_width * (len(self.sizes) - 2), max_height + 4.2,
+                                   color=(0, 0, 0, 0.08)))
+        ax.text(layer_width * (len(self.sizes)) / 2, max_height + 3,
+                "Hidden Layer" + ("s" if (len(self.sizes) - 2) > 1 else ""), va="center", ha="center")
+        ax.add_patch(
+            plt.Rectangle((layer_width + layer_width * (len(self.sizes) - 2), 0), layer_width, max_height + 4.2,
+                          color=(0, 0, 0, 0.05)))
+        ax.text(layer_width * (len(self.sizes) - 0.5), max_height + 3, "Output Layer", va="center", ha="center")
+
+    def show_perceptrons(self, ax, max_height=None, layer_width=6, base_colour=(0, 0, 0)):
+        x_offset = layer_width / 2
+
+        for x, layer in enumerate(self.sizes):
+            if layer > max_height:
+
+                ax.text(x * layer_width + x_offset, max_height + 1.4, f"{layer}", va="center", ha="center")
+
+                for y in range(max_height)[:int(max_height / 2) - 1]:
+                    ax.add_patch(plt.Circle((x * layer_width + x_offset, y + 1),
+                                            0.4,
+                                            edgecolor=base_colour,
+                                            facecolor=(base_colour, self.perceptrons[x][y])))
+
+                for i in [-0.4, 0, 0.4]:
+                    ax.add_patch(plt.Circle((x * layer_width + x_offset, (max_height + 1) / 2 + i),
+                                            0.08,
+                                            color="black"))
+
+                for y in range(max_height)[int(max_height / 2) + 1 + (max_height & 1):]:
+                    ax.add_patch(plt.Circle((x * layer_width + x_offset, y + 1),
+                                            0.4,
+                                            edgecolor=base_colour,
+                                            facecolor=(base_colour, self.perceptrons[x][y])))
+
+            else:
+
+                ax.text(x * layer_width + x_offset, max_height + 1.4, f"{layer}", va="center", ha="center")
+
+                for y in range(layer):
+                    ax.add_patch(plt.Circle((x * layer_width + x_offset, y + 1 + (max_height - layer) / 2),
+                                            0.4,
+                                            edgecolor=base_colour,
+                                            facecolor=(base_colour, self.perceptrons[x][y])))
+
+    def display(self, max_height=None, layer_width=6, base_colour=(0, 0, 0)):
         if max_height is None:
             max_height = max(self.sizes)
 
         fig, ax = plt.subplots()
-        x_offset = 1
-        for x, layer in enumerate(self.sizes):
-            if layer > max_height:
-                x_offset = 4
-                for y in range(max_height)[:int(max_height/2) - 1]:
-                    ax.add_patch(plt.Circle((x * 5 + x_offset, y + 1),
-                                            0.4,
-                                            color='red',
-                                            alpha=self.perceptrons[x][y]))
 
-                for i in [-0.4, 0, 0.4]:
-                    ax.add_patch(plt.Circle((x * 5 + x_offset, (max_height+1)/2 + i),
-                                            0.08,
-                                            color='black'))
-
-                ax.text(x * 5 + x_offset - 1, (max_height+1)/2, r"|\nabla\phi| &=& 1,\\")
-
-                for y in range(max_height)[int(max_height/2) + 1 + (max_height & 1):]:
-                    ax.add_patch(plt.Circle((x * 5 + x_offset, y + 1),
-                                            0.4,
-                                            color='red',
-                                            alpha=self.perceptrons[x][y]))
-
-            else:
-                for y in range(layer):
-                    ax.add_patch(plt.Circle((x * 5 + x_offset, y + 1 + (max_height - layer) / 2),
-                                            0.4,
-                                            color='red',
-                                            alpha=self.perceptrons[x][y]))
+        self.show_background(ax, max_height, layer_width)
+        self.show_perceptrons(ax, max_height, layer_width, base_colour)
 
         # Set limits so circles are visible
-        ax.set_xlim(0, len(self.sizes) * 5 - 3.6 + x_offset)
-        ax.set_ylim(0, max_height + 1)
+        ax.set_xlim(0, len(self.sizes) * layer_width)
+        ax.set_ylim(0, max_height + 4.2)
 
         # Ensure circles stay circular
-        ax.set_aspect('equal')
+        ax.set_aspect("equal")
 
         # Hide axes for a cleaner diagram look
-        ax.axis('on')
+        ax.axis("off")
 
-        plt.title("Multilayer Perceptron")
+        plt.title("Multilayer Perceptron", size=20)
         plt.show()
 
 
 MLP = MultilayerPerceptron([784, 16, 16, 16, 10])
-MLP.display(16)
+MLP.display(max_height=16, base_colour=(0.1, 0.4, 0.9))
